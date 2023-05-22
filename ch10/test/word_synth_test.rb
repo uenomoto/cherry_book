d1e45c7ca6047a60f9ab3ab3b928e990fda1f6bf
上野元希 <uenomotoki@uenomotomarenoMacBook-Air.local>
2023-05-22 18:23:22 +0900

    fin

---
+++ ch10/test/word_synth_test.rb
@@ -0,0 +1,27 @@
+require 'minitest/autorun'
+require_relative '../class/wordsynth'
+require_relative '../module/effects'
+
+
+class WordSynthTest < Minitest::Test
+  def test_play
+    def test_play_without_effects
+      synth = WordSynth.new
+      assert_equal 'Ruby is fun!', synth.play('Ruby is fun!')
+    end
+  end
+
+  def test_play_with_reverse
+    synth = WordSynth.new
+    synth.add_effect(Effects.reverse)
+    assert_equal 'ybuR si !nuf', synth.play('Ruby is fun!')
+  end
+
+  def test_play_with_many_effects
+    synth = WordSynth.new
+    synth.add_effect(Effects.echo(2))
+    synth.add_effect(Effects.loud(3))
+    synth.add_effect(Effects.reverse)
+    assert_equal '!!!YYBBUURR !!!SSII !!!!!NNUUFF', synth.play('Ruby is fun!')
+  end
+end
